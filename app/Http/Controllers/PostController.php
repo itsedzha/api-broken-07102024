90fe4c8e69aa4f27e0fcce34f6b5800682ec5c1f
unknown <your@email.example>
2024-10-07 15:57:40 +0300

    fixed index function

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -21,7 +21,7 @@ class PostController extends Controller implements HasMiddleware
      */
     public function index()
     {
-        return Post()->all();
+        return Post::all();
     }
 
     /**
